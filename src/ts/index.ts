5860f461d3034a5df2003ae1197dfa5dad6a3d27
laibao101 <369632567@qq.com>
2017-10-29 23:04:46 +0800

    修正

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,95 +1,176 @@
 interface treeHtml {
-    ul:HTMLElement;
-    li:HTMLElement;
+    ul : HTMLElement;
+    li : HTMLElement;
 }
 
-
 class Tree {
-    private _data:treeData[];
-    private _tree:HTMLElement;
-    private _container:HTMLElement;
-    constructor(data:treeData[]){
+    private _data : treeData[];
+    private _tree : HTMLElement = document.createElement("ul");
+    private _container : HTMLElement;
+    private _sortData : treeData[];
+    private root : treeData;
+    constructor(data : treeData[]) {
         this._data = data;
     }
 
     /**
      * 返回data
      */
-    get data(){
+    get data() {
         return this._data;
     }
 
-    get html(){
+    get html() {
         return this._tree;
     }
 
     /**
      * 创建一个树形组件
      */
-    create(){
-        const ul:HTMLElement = this._curateUl();
+    create() {
+        const ul : HTMLElement = this._curateUl();
 
         this._container = ul;
-        ul.className="tree-container";
+        ul.className = "tree-container";
 
-        this._data.forEach((treeItem)=>{
-            const li:HTMLElement = this._createLi();
-            this._container.appendChild(li);
-            li.className="tree-item";
-            li.innerText = treeItem.element;
-            li.id = `tree-item-${treeItem.id}`;
-        },this);
-        this._tree = this._container;
+        const root = this._getRoot();
+
+        console.log(root);
+        this._getTreeData();
+        // this
+        //     ._data
+        //     .forEach((treeItem) => {
+        //         const li : HTMLElement = this._createLi();
+        //         this
+        //             ._container
+        //             .appendChild(li);
+        //         li.className = "tree-item";
+        //         li.innerText = treeItem.element;
+        //         li.id = `tree-item-${treeItem.id}`;
+        //         li.setAttribute("data-pid", `${treeItem.pid}`);
+        //     }, this);
+        // this._tree = this._container;
     }
 
     /**
      * 刷新数据,重新生成一个树
      */
-    fresh(data:treeData[]){
+    fresh(data : treeData[]) {
         this._data = data;
     }
 
     /**
      * 组件的ul部分
      */
-    _curateUl():HTMLElement{
-           return document.createElement('ul');
+    _curateUl() : HTMLElement {
+        return document.createElement('ul');
     }
 
     /**
      * 组件的li部分html
      */
-    _createLi():HTMLElement{
+    _createLi() : HTMLElement {
         return document.createElement('li');
+    }
+    /**
+     * 获取root,pid最小的就是root
+     */
+    _getRoot() {
+        this._sortData = Array.from(this._data);
+        this
+            ._sortData
+            .sort((a, b) => a.pid - b.pid);
+        this.root = this._sortData[0];
+        this._sortData.splice(0,1);
+        return this.root;
+    }
+
+    /**
+     * 获取treeData
+     */
+    _getTreeData(){
+        console.log(this._getChild(this.root.pid));
+    }
+
+    /**
+     * 获取当前元素的子节点
+     */
+    _getChild(pid:number){
+        let result:treeData[]=[];
+        this._sortData.forEach((item,index) => {
+            if(item.pid === pid){
+                result.push(item);
+                this._sortData.splice(index,1);
+            }
+        });
+
+        return result;
     }
 }
 
-
 interface treeData {
-    element:string;
-    id:number;
-    pid:number;
+    element : string;
+    id : number;
+    pid : number;
 }
 
-const data:treeData[] = [  
-    {element:'轮播',id:2,pid:1},  
-    {element:'轮播小图',id:3,pid:2},  
-    {element:'首页',id:1,pid:0},  
-    {element:'轮播大图',id:4,pid:2},  
-    {element:'产品',id:5,pid:0},  
-    {element:'蔬菜',id:6,pid:5},  
-    {element:'有机蔬菜',id:7,pid:6},  
-    {element:'无机蔬菜',id:8,pid:6},  
-    {element:'水果',id:9,pid:5},  
-    {element:'有机水果',id:10,pid:9},  
-    {element:'有机大水果',id:11,pid:10},  
-    {element:'有机小水果',id:12,pid:10},  
-    {element:'无机水果',id:13,pid:9},  
-];  
-
+const data : treeData[] = [
+    {
+        element: '轮播',
+        id: 2,
+        pid: 1
+    }, {
+        element: '轮播小图',
+        id: 3,
+        pid: 2
+    }, {
+        element: '首页',
+        id: 1,
+        pid: 0
+    }, {
+        element: '轮播大图',
+        id: 4,
+        pid: 2
+    }, {
+        element: '产品',
+        id: 5,
+        pid: 0
+    }, {
+        element: '蔬菜',
+        id: 6,
+        pid: 5
+    }, {
+        element: '有机蔬菜',
+        id: 7,
+        pid: 6
+    }, {
+        element: '无机蔬菜',
+        id: 8,
+        pid: 6
+    }, {
+        element: '水果',
+        id: 9,
+        pid: 5
+    }, {
+        element: '有机水果',
+        id: 10,
+        pid: 9
+    }, {
+        element: '有机大水果',
+        id: 11,
+        pid: 10
+    }, {
+        element: '有机小水果',
+        id: 12,
+        pid: 10
+    }, {
+        element: '无机水果',
+        id: 13,
+        pid: 9
+    }
+];
 
 // 单元测试
 const tree = new Tree(data);
 tree.create();
 console.log(tree.html);
-console.log(tree.data);
--- www/js/index.js
+++ www/js/index.js
@@ -73,70 +73,190 @@
 "use strict";
 
 
-var Tree = /** @class */function () {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Tree = function () {
     function Tree(data) {
+        _classCallCheck(this, Tree);
+
+        this._tree = document.createElement("ul");
         this._data = data;
     }
-    Object.defineProperty(Tree.prototype, "data", {
+    /**
+     * 返回data
+     */
+
+
+    _createClass(Tree, [{
+        key: "create",
+
         /**
-         * 返回data
+         * 创建一个树形组件
          */
+        value: function create() {
+            var ul = this._curateUl();
+            this._container = ul;
+            ul.className = "tree-container";
+            var root = this._getRoot();
+            console.log(root);
+            this._getTreeData();
+            // this
+            //     ._data
+            //     .forEach((treeItem) => {
+            //         const li : HTMLElement = this._createLi();
+            //         this
+            //             ._container
+            //             .appendChild(li);
+            //         li.className = "tree-item";
+            //         li.innerText = treeItem.element;
+            //         li.id = `tree-item-${treeItem.id}`;
+            //         li.setAttribute("data-pid", `${treeItem.pid}`);
+            //     }, this);
+            // this._tree = this._container;
+        }
+        /**
+         * 刷新数据,重新生成一个树
+         */
+
+    }, {
+        key: "fresh",
+        value: function fresh(data) {
+            this._data = data;
+        }
+        /**
+         * 组件的ul部分
+         */
+
+    }, {
+        key: "_curateUl",
+        value: function _curateUl() {
+            return document.createElement('ul');
+        }
+        /**
+         * 组件的li部分html
+         */
+
+    }, {
+        key: "_createLi",
+        value: function _createLi() {
+            return document.createElement('li');
+        }
+        /**
+         * 获取root,pid最小的就是root
+         */
+
+    }, {
+        key: "_getRoot",
+        value: function _getRoot() {
+            this._sortData = Array.from(this._data);
+            this._sortData.sort(function (a, b) {
+                return a.pid - b.pid;
+            });
+            this.root = this._sortData[0];
+            this._sortData.splice(0, 1);
+            return this.root;
+        }
+        /**
+         * 获取treeData
+         */
+
+    }, {
+        key: "_getTreeData",
+        value: function _getTreeData() {
+            console.log(this._getChild(this.root.pid));
+        }
+        /**
+         * 获取当前元素的子节点
+         */
+
+    }, {
+        key: "_getChild",
+        value: function _getChild(pid) {
+            var _this = this;
+
+            var result = [];
+            this._sortData.forEach(function (item, index) {
+                if (item.pid === pid) {
+                    result.push(item);
+                    _this._sortData.splice(index, 1);
+                }
+            });
+            return result;
+        }
+    }, {
+        key: "data",
         get: function get() {
             return this._data;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(Tree.prototype, "html", {
+        }
+    }, {
+        key: "html",
         get: function get() {
             return this._tree;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    /**
-     * 创建一个树形组件
-     */
-    Tree.prototype.create = function () {
-        var _this = this;
-        var ul = this._curateUl();
-        this._container = ul;
-        ul.className = "tree-container";
-        this._data.forEach(function (treeItem) {
-            var li = _this._createLi();
-            _this._container.appendChild(li);
-            li.className = "tree-item";
-            li.innerText = treeItem.element;
-            li.id = "tree-item-" + treeItem.id;
-        }, this);
-        this._tree = this._container;
-    };
-    /**
-     * 刷新数据,重新生成一个树
-     */
-    Tree.prototype.fresh = function (data) {
-        this._data = data;
-    };
-    /**
-     * 组件的ul部分
-     */
-    Tree.prototype._curateUl = function () {
-        return document.createElement('ul');
-    };
-    /**
-     * 组件的li部分html
-     */
-    Tree.prototype._createLi = function () {
-        return document.createElement('li');
-    };
+        }
+    }]);
+
     return Tree;
 }();
-var data = [{ element: '轮播', id: 2, pid: 1 }, { element: '轮播小图', id: 3, pid: 2 }, { element: '首页', id: 1, pid: 0 }, { element: '轮播大图', id: 4, pid: 2 }, { element: '产品', id: 5, pid: 0 }, { element: '蔬菜', id: 6, pid: 5 }, { element: '有机蔬菜', id: 7, pid: 6 }, { element: '无机蔬菜', id: 8, pid: 6 }, { element: '水果', id: 9, pid: 5 }, { element: '有机水果', id: 10, pid: 9 }, { element: '有机大水果', id: 11, pid: 10 }, { element: '有机小水果', id: 12, pid: 10 }, { element: '无机水果', id: 13, pid: 9 }];
+
+var data = [{
+    element: '轮播',
+    id: 2,
+    pid: 1
+}, {
+    element: '轮播小图',
+    id: 3,
+    pid: 2
+}, {
+    element: '首页',
+    id: 1,
+    pid: 0
+}, {
+    element: '轮播大图',
+    id: 4,
+    pid: 2
+}, {
+    element: '产品',
+    id: 5,
+    pid: 0
+}, {
+    element: '蔬菜',
+    id: 6,
+    pid: 5
+}, {
+    element: '有机蔬菜',
+    id: 7,
+    pid: 6
+}, {
+    element: '无机蔬菜',
+    id: 8,
+    pid: 6
+}, {
+    element: '水果',
+    id: 9,
+    pid: 5
+}, {
+    element: '有机水果',
+    id: 10,
+    pid: 9
+}, {
+    element: '有机大水果',
+    id: 11,
+    pid: 10
+}, {
+    element: '有机小水果',
+    id: 12,
+    pid: 10
+}, {
+    element: '无机水果',
+    id: 13,
+    pid: 9
+}];
 // 单元测试
 var tree = new Tree(data);
 tree.create();
 console.log(tree.html);
-console.log(tree.data);
 
 /***/ })
 /******/ ]);
